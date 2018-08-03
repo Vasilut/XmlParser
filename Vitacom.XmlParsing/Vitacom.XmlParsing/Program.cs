5064a0e0b7067c41eaf12eb39398b0dcfbbeea81
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-08-03 12:56:46 +0300

    s

--- Vitacom.XmlParsing/Vitacom.XmlParsing/Program.cs
+++ Vitacom.XmlParsing/Vitacom.XmlParsing/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace Vitacom.XmlParsing
 {
@@ -6,7 +8,21 @@ namespace Vitacom.XmlParsing
     {
         static void Main(string[] args)
         {
+            XDocument document = XDocument.Load(@"D:\download\nedis_catalog_2018-07-18_en_US_58960_v1-0_xml.xml\nedis_catalog_2018-07-18_en_US_58960_v1-0_xml.xml");
+
+            var productList = from prod in document.Descendants("product") select prod;
+            foreach (var item in productList)
+            {
+                foreach (var descendant in item.Descendants())
+                {
+                    if(descendant.HasElements)
+                    {
+
+                    }
+                }
+            }
             Console.WriteLine("Hello World!");
+            var x = 2;
             Console.ReadLine();
         }
     }
