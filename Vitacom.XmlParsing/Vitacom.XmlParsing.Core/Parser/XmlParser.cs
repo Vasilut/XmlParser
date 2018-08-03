4916005b7db8e7be1c598659c0caf3928dbf54b6
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-08-03 16:08:45 +0300

    Removed code from program.cs and added new logic.

---
+++ Vitacom.XmlParsing/Vitacom.XmlParsing.Core/Parser/XmlParser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Vitacom.XmlParsing.Core.Factory;
+using Vitacom.XmlParsing.Model;
+
+namespace Vitacom.XmlParsing.Core.Parser
+{
+    public class XmlParser : IXmlParser
+    {
+
+        public XmlParser()
+        {
+        }
+        public List<Product> ParseXmlDocument(string inputFilePath)
+        {
+            List<Product> _productList = new List<Product>();
+            XDocument document = XDocument.Load(inputFilePath);
+
+            var productList = from prod in document.Descendants("product") select prod;
+            foreach (var item in productList)
+            {
+                //iteram fiecare produs
+                //luam proprietatile simple
+                var product = new Product
+                {
+                    NedisPartnr = item.Element("nedisPartnr").Value,
+                    NedisArtId = item.Element("nedisArtlid").Value,
+                    VendorPartnr = item.Element("vendorPartnr").Value,
+                    Brand = item.Element("brand").Value,
+                    Ean = item.Element("EAN").Value,
+                    InstraStatCode = item.Element("intrastatCode").Value,
+                    Unspsc = item.Element("UNSPSC").Value,
+                    HeaderText = item.Element("headerText").Value,
+                    InternetText = item.Element("internetText").Value,
+                    GeneralText = item.Element("generalText").Value
+                };
+
+                var complexNodes = item.Descendants().Where(node => node.HasElements == true).ToList();
+                foreach (var descendant in complexNodes)
+                {
+                    //iteram fiecare proprietate complexa a produsului
+
+                    var propertyInstance = PropertyFactory.GetProperty(descendant.Name.LocalName);
+                    if (propertyInstance != null)
+                    {
+                        var complexPropertyParser = new ComplexPropertyParser(propertyInstance);
+                        complexPropertyParser.ParseComplexProperty(ref product, descendant);
+                    }
+                    
+                }
+                _productList.Add(product);
+            }
+
+            return _productList;
+        }
+    }
+}
--- Vitacom.XmlParsing/Vitacom.XmlParsing/Program.cs
+++ Vitacom.XmlParsing/Vitacom.XmlParsing/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
+using Vitacom.XmlParsing.Core.Parser;
 using Vitacom.XmlParsing.Model;
 
 namespace Vitacom.XmlParsing
@@ -9,74 +10,67 @@ namespace Vitacom.XmlParsing
     
     public class Program
     {
-        static void Rec(XElement element)
-        {
-            if(element.HasElements)
-            {
-                foreach (var el in element.Descendants())
-                {
-                    Rec(el);
-                }
-            }
-
-            Console.WriteLine(element.Value);
-        }
         
         static void Main(string[] args)
         {
             List<Product> productLst = new List<Product>();
-            XDocument document = XDocument.Load(@"D:\download\nedis_catalog_2018-07-18_en_US_58960_v1-0_xml.xml\fis.xml");
+            string xmlFilePath = @"D:\download\nedis_catalog_2018-07-18_en_US_58960_v1-0_xml.xml\fis.xml";
+            var xmlParser = new XmlParser();
+            productLst = xmlParser.ParseXmlDocument(xmlFilePath);
 
-            var productList = from prod in document.Descendants("product") select prod;
-            foreach (var item in productList)
-            {
-                //iteram fiecare produs
-                //luam proprietatile simple
-                var product = new Product
-                {
-                    NedisPartnr = item.Element("nedisPartnr").Value,
-                    NedisArtId = item.Element("nedisArtlid").Value,
-                    VendorPartnr = item.Element("vendorPartnr").Value,
-                    Brand = item.Element("brand").Value,
-                    Ean = item.Element("EAN").Value,
-                    InstraStatCode = item.Element("intrastatCode").Value,
-                    Unspsc = item.Element("UNSPSC").Value,
-                    HeaderText = item.Element("headerText").Value,
-                    InternetText = item.Element("internetText").Value,
-                    GeneralText = item.Element("generalText").Value
-                };
 
-                var complexNodes = item.Descendants().Where(node => node.HasElements == true).ToList();
-                foreach (var descendant in complexNodes)
-                {
-                    //iteram fiecare proprietate complexa a produsului
-                    if(descendant.Name.LocalName == "images")
-                    {
-                        foreach (var itemj in descendant.Descendants())
-                        {
-                            Console.WriteLine(itemj.Value);
-                            Console.Write(itemj.Attribute("order").Value);
-                            Console.WriteLine(itemj);
-                        }
-                    }
-                    else
-                    if(descendant.Name.LocalName == "categories")
-                    {
-                        var xxl = descendant.Descendants();
-                    }
-                    else
-                    if(descendant.Name.LocalName == "tree")
-                    {
-                        var xxl = descendant.Descendants();
-                    }
-                    else
-                    if(descendant.Name.LocalName == "priceLevels")
-                    {
-                        var xxl = descendant.Descendants();
-                    }
-                }
-                productLst.Add(product);
-            }
+            //XDocument document = XDocument.Load(@"D:\download\nedis_catalog_2018-07-18_en_US_58960_v1-0_xml.xml\fis.xml");
+
+            //var productList = from prod in document.Descendants("product") select prod;
+            //foreach (var item in productList)
+            //{
+            //    //iteram fiecare produs
+            //    //luam proprietatile simple
+            //    var product = new Product
+            //    {
+            //        NedisPartnr = item.Element("nedisPartnr").Value,
+            //        NedisArtId = item.Element("nedisArtlid").Value,
+            //        VendorPartnr = item.Element("vendorPartnr").Value,
+            //        Brand = item.Element("brand").Value,
+            //        Ean = item.Element("EAN").Value,
+            //        InstraStatCode = item.Element("intrastatCode").Value,
+            //        Unspsc = item.Element("UNSPSC").Value,
+            //        HeaderText = item.Element("headerText").Value,
+            //        InternetText = item.Element("internetText").Value,
+            //        GeneralText = item.Element("generalText").Value
+            //    };
+
+            //    var complexNodes = item.Descendants().Where(node => node.HasElements == true).ToList();
+            //    foreach (var descendant in complexNodes)
+            //    {
+            //        //iteram fiecare proprietate complexa a produsului
+            //        if(descendant.Name.LocalName == "images")
+            //        {
+            //            foreach (var itemj in descendant.Descendants())
+            //            {
+            //                Console.WriteLine(itemj.Value);
+            //                Console.Write(itemj.Attribute("order").Value);
+            //                Console.WriteLine(itemj);
+            //            }
+            //        }
+            //        else
+            //        if(descendant.Name.LocalName == "categories")
+            //        {
+            //            var xxl = descendant.Descendants();
+            //        }
+            //        else
+            //        if(descendant.Name.LocalName == "tree")
+            //        {
+            //            var xxl = descendant.Descendants();
+            //        }
+            //        else
+            //        if(descendant.Name.LocalName == "priceLevels")
+            //        {
+            //            var xxl = descendant.Descendants();
+            //        }
+            //    }
+            //    productLst.Add(product);
+            //}
 
 
             Console.WriteLine("Hello World!");
