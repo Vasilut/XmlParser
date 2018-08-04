ff1420d591724b9e7aacf8cfcbb82d0540d63862
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-08-04 11:40:03 +0300

    Added new property (status + properties).

--- Vitacom.XmlParsing/Vitacom.XmlParsing.Core/Strategy/PropertiesProperty.cs
+++ Vitacom.XmlParsing/Vitacom.XmlParsing.Core/Strategy/PropertiesProperty.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
 using Vitacom.XmlParsing.Model;
+using Vitacom.XmlParsing.Model.Model;
 
 namespace Vitacom.XmlParsing.Core.Strategy
 {
     public class PropertiesProperty : PropertyStrategy
     {
+        private List<Properties> _propertiList;
+
+        public PropertiesProperty()
+        {
+            _propertiList = new List<Properties>();
+        }
         public override void ProcessProperty(ref Product product, XElement property)
         {
-            throw new NotImplementedException();
+            foreach (var item in property.Descendants())
+            {
+                var propertyNew = new Properties();
+                propertyNew.Id = item.Attribute("id")?.Value;
+                propertyNew.MultiplierId = item.Attribute("multiplierID")?.Value;
+                propertyNew.UnitId = item.Attribute("unitID")?.Value;
+                propertyNew.ValueId = item.Attribute("valueID")?.Value;
+                propertyNew.PropertiValue = item.Value;
+                _propertiList.Add(propertyNew);
+            }
+            product.Properties = _propertiList;
         }
     }
 }
--- Vitacom.XmlParsing/Vitacom.XmlParsing.Core/Strategy/StatusProperty.cs
+++ Vitacom.XmlParsing/Vitacom.XmlParsing.Core/Strategy/StatusProperty.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
 using Vitacom.XmlParsing.Model;
+using Vitacom.XmlParsing.Model.Model;
 
 namespace Vitacom.XmlParsing.Core.Strategy
 {
     public class StatusProperty : PropertyStrategy
     {
+        private List<Status> _statusList;
+
+        public StatusProperty()
+        {
+            _statusList = new List<Status>();
+        }
         public override void ProcessProperty(ref Product product, XElement property)
         {
-            throw new NotImplementedException();
+            foreach (var item in property.Descendants())
+            {
+                var status = new Status();
+                status.StatusAttribute = item.Attribute("status")?.Value;
+                status.Value = item.Value;
+                _statusList.Add(status);
+            }
+            product.Statuses = _statusList;
         }
     }
 }
